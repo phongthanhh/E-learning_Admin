6eea7c6ef3358dcec277e4d6529412fb6812dec9
Tai Vo <tantaivo74@gmail.com>
2023-04-05 23:15:27 +0700

    chore: add react-toastify css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+// Toastify
+import 'react-toastify/dist/ReactToastify.css'
 
 const queryClient = new QueryClient()
 
