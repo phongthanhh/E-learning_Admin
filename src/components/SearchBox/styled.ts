a5f84296047169c34594aa88ede4f2349f314233
Tai Vo <tantaivo74@gmail.com>
2023-04-09 17:32:18 +0700

    feat(components): add SearchBox

---
+++ src/components/SearchBox/styled.ts
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material'
+import styled from 'styled-components'
+
+export const StyledContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 1rem;
+`
+
+export const StyledChildren = styled(Box)`
+  padding: 0 10rem;
+`
+
+export const StyledButtons = styled(Box)`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`
