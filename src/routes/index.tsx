63f26fb250c3a00ba280c8fc00bea4a5e455fd04
Tai Vo <tantaivo74@gmail.com>
2023-04-09 15:00:19 +0700

    fix: add fallback loading for suspense

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
+import { Loading } from 'components'
+import SyncUser from 'pages/sync-user'
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router'
 
 const AdminLayout = lazy(() => import('../layouts/admin'))
 const User = lazy(() => import('../pages/users'))
-const SyncUser = lazy(() => import('../pages/sync-user'))
 
 function AppRoutes() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <Routes>
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<User />} />
