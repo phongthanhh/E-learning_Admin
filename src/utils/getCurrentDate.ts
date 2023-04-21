e1f999adbc4f29cc686839e21fce151f514331cd
Thanh Phong <tangthanh.phong99@gmail.com>
2023-04-21 13:00:28 +0700

    feat: add get current date func

---
+++ src/utils/getCurrentDate.ts
@@ -0,0 +1,8 @@
+export function getCurrentDate(separator = '') {
+  const newDate = new Date()
+  const date = newDate.getDate()
+  const month = newDate.getMonth() + 1
+  const year = newDate.getFullYear()
+
+  return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date}`
+}
