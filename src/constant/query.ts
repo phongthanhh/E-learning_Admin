b2081e4daa89a96472786eff1f23758eb64c63f1
Thanh Phong <tangthanh.phong99@gmail.com>
2023-04-21 13:33:59 +0700

    chore: defind query key & storage

--- src/constant/query.ts
+++ src/constant/query.ts
@@ -1,4 +1,6 @@
 export const QUERY_KEY = {
   USERS: 'users',
-  MEMBER_TYPES: 'memberTypes'
+  MEMBER_TYPES: 'memberTypes',
+  COURSES: 'courses',
+  COURSE_CATE: 'courseCate'
 }
--- src/constant/storage.ts
+++ src/constant/storage.ts
@@ -1,3 +1,4 @@
 export enum Storage {
-  ACCESS_TOKEN = 'ACCESS_TOKEN'
+  ACCESS_TOKEN = 'ACCESS_TOKEN',
+  USER_LOGIN = 'USER_LOGIN'
 }
