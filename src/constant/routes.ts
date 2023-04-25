c6842b74ecf0849ba2becf62c053152cf3710af3
Thanh Phong <tangthanh.phong99@gmail.com>
2023-04-25 15:10:34 +0700

    feat: defind route name constant

---
+++ src/constant/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES_NAME = {
+  ROOT: '/',
+  ADMIN: '/admin',
+  COURSE: '/admin/course',
+  REGISTER: '/admin/register',
+  USERS_REGISTER: '/admin/register/users',
+  COURSE_REGISTER: '/admin/register/courses',
+  SYNC_USER: '/sync-user'
+}
