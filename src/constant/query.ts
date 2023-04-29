15b0e8135f9feeb1d7b2beb07730f664264f43b4
Thanh Phong <tangthanh.phong99@gmail.com>
2023-04-29 18:22:33 +0700

    feat: defind query key in constant

--- src/constant/query.ts
+++ src/constant/query.ts
@@ -3,7 +3,10 @@ export const QUERY_KEY = {
   MEMBER_TYPES: 'memberTypes',
   COURSES: 'courses',
   COURSE_CATE: 'courseCate',
-  REGISTER_USERS: 'registerUser',
-  UNREGISTER_USERS: 'unregisterUser',
-  WAITING_USERS: 'waitingUsers'
+  REGISTERED_USERS: 'registerUser',
+  UNREGISTERED_USERS: 'unregisterUser',
+  WAITING_USERS: 'waitingUsers',
+  APPROVED_COURSE: 'approvedCourse',
+  UNREGISTERED_USERS_COURSE: 'unregisteredUserCourse',
+  COURSES_OF_USER_WAITING_APPROVAL: 'coursesOfUserWaitingApproval'
 }
