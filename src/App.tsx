2f47bc98780288617105ce56ae63ef24898296e5
Thanh Phong <tangthanh.phong99@gmail.com>
2023-05-14 17:02:39 +0700

    fix: get admin info

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@
 import AppRoutes from 'routes'
 import { Toast } from 'components'
 import { useQuery } from '@tanstack/react-query'
-import { QUERY_KEY, Storage } from 'constant'
+import { QUERY_KEY } from 'constant'
 import { getAdminInfoApi } from 'services'
 
 function App() {
-  const accessTokenLocal = localStorage.getItem(Storage.ACCESS_TOKEN)
   const adminInfoQuery = useQuery({
     queryKey: [QUERY_KEY.ADMIN_INFO],
     queryFn: () => getAdminInfoApi(),
-    enabled: Boolean(accessTokenLocal),
     retry: 0
   })
   return (
